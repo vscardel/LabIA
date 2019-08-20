0b45f2474d1f10bef6a56ce8431356ca31bed0f7
vscardel <37451164+vscardel@users.noreply.github.com>
2019-08-20 11:13:54 -0400

    Add files via upload

--- praticaLabIA.py
+++ praticaLabIA.py
@@ -2,54 +2,70 @@ import os
 import numpy as np
 import cv2
 
-path = '/tmp/guest-apin3n/base'
+def aplica_peso(n,peso):
+	n = n * peso
+	return int(n)
 
-num_treino = 0
-num_teste = 0
 
-#numero de arquivos no folder de treino e no folder de teste
-for directory in os.listdir(path):
-	if directory == 'treino':
-		for dir_treino in os.listdir(path + '/' + directory):
-			num_treino = num_treino + len(os.listdir(path + '/' + directory + '/' + dir_treino))
-	else:
-		num_teste = num_teste + len(os.listdir(path + '/' + directory))
+def processa_base(path,heigth,width,dimension,size_of_validation):
 
-treino = np.empty([num_treino,64,64,3], dtype=np.uint8)
-teste = np.empty([num_teste,64,64,3], dtype=np.uint8)
-labels = np.empty([num_treino], dtype = np.uint8)
-validation = np.empty([num_treino/4], dtype = np.uint8)
+	num_treino = 0
+	num_teste = 0
 
-#preenchendo os vetores de treino e de teste
-for directory in os.listdir(path):
-	if directory == 'treino':
-		k = 0
-		l = 0
-		for dir_treino in os.listdir(path + '/' + directory):
-			for img in os.listdir(path + '/' + directory + '/' + dir_treino):
+	#numero de arquivos no folder de treino e no folder de teste
+	for directory in os.listdir(path):
+		if directory == 'treino':
+			for dir_treino in os.listdir(path + '/' + directory):
+				num_treino = num_treino + len(os.listdir(path + '/' + directory + '/' + dir_treino))
+		else:
+			num_teste = num_teste + len(os.listdir(path + '/' + directory))
 
-				classe = 0
-				if dir_treino == 'sinuca':
-					classe = 0
-				elif dir_treino == 'futebol':
-					classe = 1
-				elif dir_treino == 'tenis':
-					classe = 2
-				elif dir_treino == 'golfe':
-					classe = 3					
-					
-				img = cv2.imread(path + '/' + directory + '/' + dir_treino + '/' + img,cv2.IMREAD_COLOR)
-				treino[k] = img
-				labels[l] = classe
+	#criando os vetores
+	num_exemplos_por_classe = []
+	treino = np.empty([num_treino,heigth,width,dimension], dtype=np.uint8)
+	teste = np.empty([num_teste,heigth,width,dimension], dtype=np.uint8)
+	labels_treino = np.empty([num_treino], dtype = np.uint8)
+	labels_validation = np.empty([int(num_treino*size_of_validation)], dtype = np.uint8)
+	validation = np.empty([int(num_treino*size_of_validation),heigth,width,dimension],dtype=np.uint8)
+
+	#preenchendo os vetores de treino e de teste
+	for directory in os.listdir(path):
+		if directory == 'treino':
+			k = 0
+			l = 0
+			for classe,dir_treino in enumerate(os.listdir(path + '/' + directory)):
+				lista_imagens_classe = os.listdir(path + '/' + directory + '/' + dir_treino)
+				num_exemplos_por_classe.append(len(lista_imagens_classe))
+				for img in lista_imagens_classe:
+	
+					imagem = cv2.imread(path + '/' + directory + '/' + dir_treino + '/' + img,cv2.IMREAD_COLOR)
+					treino[k] = imagem
+					labels_treino[l] = classe
+					k = k + 1
+					l = l + 1
+		else:
+			k = 0
+			for img in os.listdir(path + '/' + directory):
+				imagem = cv2.imread(path + '/' + directory + '/' + img,cv2.IMREAD_COLOR)
+				teste[k] = imagem
 				k = k + 1
-				l = l + 1
-	else:
-		k = 0
-		for img in os.listdir(path + '/' + directory):
-			img = cv2.imread(path + '/' + directory + '/' + img,cv2.IMREAD_COLOR)
-			teste[k] = img
-			k = k + 1
+	print(treino[245])
 
+	#definindo o numero de imagens para cada classe no vetor de validacao
+	# num_exemplos_por_classe = list(map(lambda i:int(i*size_of_validation),num_exemplos_por_classe))
+	# for classe,i in enumerate(num_exemplos_por_classe):
+	# 	num = 1
+	# 	v = 0
+	# 	for k in range(0,len(treino)):
+	# 		if labels_treino[k] == classe and num <= i:
+	# 			validation[v] = treino[k]
+	# 			labels_validation[v] = classe
+	# 			np.delete(treino,k)
+	# 			np.delete(labels_treino,k)
+	# 			v = v + 1
+	# 			num = num + 1
+	# 		else:
+	# 			break 		
 
-
-
+path = '/home/victor/base'
+bla = processa_base(path,64,64,3,0.2)
