74fc1a6e3c1b2722f9d2253dd2a88f030db3425e
vscardel <37451164+vscardel@users.noreply.github.com>
2019-08-28 01:43:56 -0300

    Add files via upload

--- praticaLabIA.py
+++ praticaLabIA.py
@@ -59,26 +59,44 @@ def constroi_validacao():
 			j = j + 1 
 
 def processa_base():
-
 	global tam_classes
-
 	preenche_vetores_de_treino_e_teste()
 	#aplicando os pesos para cada classe
 	tam_classes = map(lambda i: int(i*size_of_validation),tam_classes)
 	constroi_validacao()
 
-	retorno = {}
-	retorno['treino'] = treino_oficial
-	retorno['labels_treino'] = labels_treino_oficial
-	retorno['teste'] = teste
-	retorno['validacao'] = validacao
-	retorno['labels_validacao'] = labels_validacao
+def gradient_descent_step(imagens,labels,pesos,learning_rate,bias):
+	P = len(imagens)
+	N = len(pesos)
+	der_par_atual = 0
+	bias_g = 0
+	gradiente = np.empty([N])
+	for j in range(N):
+		for i in range(P):
+			features = np.reshape(imagens[i],-1)
+			der_par_atual += (2.0/P)*(np.dot(features,pesos) + bias - labels[i])*features[j]
+			if j == 0:
+				bias_g += (2.0/P)*(np.dot(features,pesos) + bias - labels[i])
+		gradiente[j] = der_par_atual
 
-	return retorno
+	bias_update = bias - (learning_rate*bias_g)
+	pesos_update = pesos - (learning_rate*gradiente)
+
+	return bias_update,pesos_update
+
+def MSE(pesos,labels,batch,bias):
+	N = len(batch)
+	erro = 0
+	for cont,img in enumerate(batch):
+		features = np.reshape(img,-1)
+		y_ = np.dot(features,pesos) + bias
+		erro += (labels[cont]-y_)**2
+	erro /= N
+	return erro
 
 ###############GLOBALS#################
 
-path = '/home/victor/base'
+path = '/home/tomas/base'
 heigth = 64
 width = 64
 dimension = 3
@@ -94,10 +112,26 @@ teste = np.empty([num_teste,heigth,width,dimension], dtype=np.uint8)
 labels_treino = np.empty([num_treino], dtype = np.uint8)
 tam_validacao = int(size_of_validation*num_treino)
 tam_treino_oficial = num_treino - tam_validacao
+tam_classes = []
+
 validacao = np.empty([tam_validacao,heigth,width,dimension], dtype=np.uint8)
 labels_validacao = np.empty([tam_validacao],dtype=np.uint8)
 treino_oficial = np.empty([tam_treino_oficial,heigth,width,dimension], dtype=np.uint8)
 labels_treino_oficial = np.empty([tam_treino_oficial], dtype = np.uint8)
-tam_classes = []
 
-vetor_de = processa_base()
+#############PARAMETROS#################
+
+pesos = np.empty([heigth*width*dimension])
+learning_rate = 0.001
+bias = 0
+batch_size = 20
+
+########################################
+
+processa_base()
+for i in range(10):
+	lista_indices = np.random.permutation(len(treino_oficial))
+	batch = np.take(treino_oficial,lista_indices[:batch_size],axis=0)
+	labels_batch = np.take(labels_treino_oficial,lista_indices[:batch_size],axis=0)
+	novo_bias,novos_pesos = gradient_descent_step(batch,labels_batch,pesos,learning_rate,bias)
+	print(MSE(novos_pesos,labels_treino_oficial,treino_oficial,novo_bias))
